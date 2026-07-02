3ceb87b40496fd3457a535bd8cac2d9b298a2768
MahdiNajafzadeh <Mahdi_Najafzadeh_1383@outlook.com>
2026-07-02 21:12:19 +0330

    UPDATE: implement api handlers

---
+++ src/api.py
@@ -0,0 +1,46 @@
+import uvicorn
+from fastapi import FastAPI
+from database import File
+from loop import Code, loop
+from config import config
+
+app = FastAPI()
+
+
+@app.get("/download")
+async def get_all_download():
+    return loop.request(Code.REQ_FILE_GET_ALL)
+
+
+@app.get("/download/{id}")
+async def get_download(id: int):
+    return loop.request(Code.REQ_FILE_GET, id)
+
+
+@app.post("/download")
+async def create_download(download: File):
+    return loop.request(Code.REQ_FILE_CREATE, download)
+
+
+@app.patch("/download/{id}")
+async def update_download(id: int, download: File):
+    return loop.request(Code.REQ_FILE_UPDATE, id, download)
+
+
+@app.delete("/download/{id}")
+async def delete_download(id: int):
+    return loop.request(Code.REQ_FILE_DELETE, id)
+
+
+@app.post("/download/{id}/start")
+async def start_download(id: int):
+    return loop.request(Code.REQ_FILE_START, id)
+
+
+@app.post("/download/{id}/stop")
+async def stop_download(id: int):
+    return loop.request(Code.REQ_FILE_STOP, id)
+
+
+def init():
+    uvicorn.run(app=app, host=str(config["HOST"]), port=int(config["PORT"]))
