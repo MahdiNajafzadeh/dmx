517f3c7f2ff2b553bf7f497841ffe2be4ad3dd63
MahdiNajafzadeh <Mahdi_Najafzadeh_1383@outlook.com>
2026-07-02 21:13:46 +0330

    UPDATE: implement bootstap script for project

---
+++ src/main.py
@@ -0,0 +1,29 @@
+"""
+name: main.py
+description: bootstrap all components
+"""
+
+from threading import Thread
+from loop import loop
+from api import init as api_init
+from handler import init as handler_init
+from log import log
+
+
+threads: list[Thread] = [
+    Thread(name="evet-loop", target=loop.loop),
+    Thread(name="api", target=api_init),
+    Thread(name="download", target=handler_init),
+]
+
+
+def main():
+    for thread in threads:
+        log.info(f"start new thread {thread.name}")
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    main()
